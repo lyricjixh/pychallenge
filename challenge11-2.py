5ba56652c030187db277a6b03304cdaad7780d84
lyricjixh <lyricji@gmail.com>
2016-07-22 15:06:41 -0400

    Update challenge11-2.py

--- challenge11-2.py
+++ challenge11-2.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python
 # PIL.Image.open(fp, mode='r')
-# Image.open().size()
+# PIL.Image.size, Image size, in pixels. The size is given as a 2-tuple (width, height).
 # Image.open().load(),  the file remains open and the actual image data is not read from the file until you try to process the data (or call the load()
-# PIL.Image.new(mode, size, color=0)
+# PIL.Image.new(mode, size, color=0), Creates a new image with the given mode and size.
+# Image.show(title=None, command=None), Displays this image. This method is mainly intended for debugging purposes.
 # http://pillow.readthedocs.io/en/3.1.x/reference/Image.html#functions
 
 from PIL import Image, ImageDraw
@@ -20,6 +21,7 @@ pixels_result = result.load()
 
 for i in range(width):
  for j in range(height):
+  # a%b is the reminder of a/b
   if i%2==0 and j%2==0:
    pixels_result[i/2, j/2] = pixels[i, j]
   elif i%2==0 and j%2==1:
